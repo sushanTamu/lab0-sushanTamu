83788ac4834691f6e20318e9fc72a1e24f4685b0
Sushan Patel <sushan_1201@hera3.ece.tamu.edu>
2023-08-28 10:25:54 -0500

    lab0_1

---
+++ lab0/intro_ver_proj_lab0/sim/xcelium.d/run.lnx86.22.03.d/files.ts
@@ -0,0 +1,2 @@
+1472525284 /home/grads/s/sushan_1201/lab0-sushanTamu/lab0/intro_ver_proj_lab0/design/common/addr_segregator_proc.sv
+1693091425 /home/grads/s/sushan_1201/lab0-sushanTamu/lab0/intro_ver_proj_lab0/tb/tb_addr_segregator_proc.sv
